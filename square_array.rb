946d547cbf5d03c0bcc730cca9421e689dff0914
github username <github email address>
2018-08-15 14:14:29 -0400

    Practiced using each method.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
+  sq_array = Array.new
+  index = 0
   # your code here
+  array.each do |element|
+    sq_array[index] = element * element
+    index += 1
+  end
+  sq_array
 end
